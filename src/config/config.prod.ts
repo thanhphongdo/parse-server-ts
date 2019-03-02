796afe65b56084bdb4f4365346498e08595ca742
Phong Do <phongdo@innoria.com>
2019-03-02 09:48:13 +0700

    init

---
+++ src/config/config.prod.ts
@@ -0,0 +1,8 @@
+import { ConfigInterface } from './config_interface';
+
+export var Config:ConfigInterface = {
+    parseServer:{},
+    scrapy: {
+        scrapyd: 'http://localhost:6800'
+    }
+}
---
+++ src/config/config_interface.ts
@@ -0,0 +1,20 @@
+export interface ConfigInterface {
+    parseServer: {
+        appName?: string;
+        databaseURI?: string;
+        cloud?: string;
+        appId?: string;
+        masterKey?: string;
+        port?: number;
+        serverURL?: string;
+        publicServerURL?: string;
+        liveQuery?: {
+            classNames: string[];
+        },
+        maxUploadSize?: string;
+    };
+    [key: string]: any;
+    scrapy: {
+        scrapyd: string;
+    }
+}
