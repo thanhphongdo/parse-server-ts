cd7633213b09b2c3e714ab03d96a96f5d9be3c0d
Phong Do <phong.do@zaitenllc.com>
2021-03-01 15:47:16 +0700

    base trigger

--- src/parse/cloud_trigger_base.ts
+++ src/parse/cloud_trigger_base.ts
@@ -1,13 +1,43 @@
 import { CloudBase } from './cloud_base';
 
-export interface CloudTrigger {
+export interface CloudTriggerBeforeSave {
     (request: Parse.Cloud.BeforeSaveRequest<Parse.Object<Parse.Attributes>>): Promise<any>;
 }
 
+export interface CloudTriggerAfterSave {
+    (request: Parse.Cloud.AfterSaveRequest<Parse.Object<Parse.Attributes>>): Promise<any>;
+}
+
+export interface CloudTriggerBeforeDelete {
+    (request: Parse.Cloud.BeforeDeleteRequest<Parse.Object<Parse.Attributes>>): Promise<any>;
+}
+
+export interface CloudTriggerAfterDelete {
+    (request: Parse.Cloud.AfterDeleteRequest<Parse.Object<Parse.Attributes>>): Promise<any>;
+}
+
 export class CloudTriggerBase extends CloudBase {
-    async defineTriggerBeforeSave(objectName: string, cloudTriggerBeforeSave: CloudTrigger) {
+    async defineTriggerBeforeSave(objectName: string, cloudTriggerBeforeSave: CloudTriggerBeforeSave) {
         Parse.Cloud.beforeSave(objectName, async (request) => {
             await cloudTriggerBeforeSave(request);
+        });
+    }
+
+    async defineTriggerAfterSave(objectName: string, cloudTriggerAfterSave: CloudTriggerAfterSave) {
+        Parse.Cloud.afterSave(objectName, async (request) => {
+            await cloudTriggerAfterSave(request);
+        });
+    }
+
+    async defineTriggerBeforeDelete(objectName: string, cloudTriggerBeforeDelete: CloudTriggerBeforeDelete) {
+        Parse.Cloud.beforeDelete(objectName, async (request) => {
+            await cloudTriggerBeforeDelete(request);
+        });
+    }
+
+    async defineTriggerAfterDelete(objectName: string, cloudTriggerAfterDelete: CloudTriggerAfterDelete) {
+        Parse.Cloud.afterDelete(objectName, async (request) => {
+            await cloudTriggerAfterDelete(request);
         });
     }
 }
