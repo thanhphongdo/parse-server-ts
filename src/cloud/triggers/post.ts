c632004e54a80fe44b63c176c1978d0e7775758e
Phong Do <phong.do@zaitenllc.com>
2021-03-01 15:51:19 +0700

    update trigger

--- src/cloud/triggers/post.ts
+++ src/cloud/triggers/post.ts
@@ -3,10 +3,10 @@ import { CloudTriggerBase } from '../../parse/index';
 export class PostCloud extends CloudTriggerBase {
     constructor() {
         super();
-        this.defineTriggerBeforeSave('Post', this.beforeSavePost);
+        this.defineTriggerAfterSave('Post', this.afterSavePost);
     }
 
-    async beforeSavePost(request: Parse.Cloud.BeforeSaveRequest<Parse.Object<Parse.Attributes>>) {
+    async afterSavePost(request: Parse.Cloud.BeforeSaveRequest<Parse.Object<Parse.Attributes>>) {
         if (!request.object.get('view')) {
             request.object.set('view', 1);
             return await request.object.save(null, { useMasterKey: true });
